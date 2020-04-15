cfd9bd058fdedd4f5066386c0e1c82ee90872aeb
Moh. Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2020-04-16 05:28:57 +0700

    finish registration module

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,18 @@
 const index = new Vue({
     el: '#app',
     data: { 
+        url: 'http://localhost:8000',
         title: 'foodiew',
         isLogin: '',
         email: '',
         password: '',
         confirm_password: '',
-        items: []
+        items: [],
+        emailError: '',
+        passwordError: '',
+        confirmPasswordError: '',
+        isLoading: false,
+        generalErrorMessage: ''
     },
     created () {
         let token = localStorage.getItem('token')
@@ -17,37 +23,60 @@ const index = new Vue({
     },
     methods: {
         register: function () {
+            this.isLoading = true
             if (this.password !== this.confirm_password) {
-                alert ('Pastikan anda memasukkan password yang sama!')
+                this.passwordError = 'Your password not match with Confirmation Password'
+                this.confirmPasswordError = 'Your Confirmation Password not match with Password'
+                setTimeout(() => {
+                    this.passwordError = ''
+                    this.confirmPasswordError = ''
+                }, 3000);
+                this.isLoading = false
                 return
             }
-            this.isLogin = 1
-            $('#staticBackdrop').modal('hide')
-            $('#staticBackdrop2').modal('hide')
-            // let url = 'https://api.foodiew.com/login'
+            // this.isLogin = 1
+            // $('#staticBackdrop').modal('hide')
+            // $('#staticBackdrop2').modal('hide')
+            let url = this.url + '/api/registration'
             // let token = localStorage.getItem('token')
-            // let header = {
-            //     headers: {
-            //         'Authorization': `${token}`,
-            //     }
-            // }
-            // let payload = {
-            //     email: this.email,
-            //     password: this.password,
-            // }
-            // axios.post(url, payload, header)
-            //     .then((res) => {
-            //         if (res.status === 200) {
-            //             this.isLogin = 1
-            //             $('#staticBackdrop').modal('hide')
-            //             $('#staticBackdrop2').modal('hide')
-            //         }
-            //     })
-            //     .catch((err) => {
-            //         $('#staticBackdrop').modal('hide')
-            //         $('#staticBackdrop2').modal('hide')
-            //         console.log(err)
-            //     })
+            let header = {
+                // headers: {
+                //     'Authorization': `${token}`,
+                // }
+            }
+            let payload = {
+                email: this.email,
+                password: this.password,
+            }
+            axios.post(url, payload, header)
+                .then((res) => {
+                    console.log(res)
+                    this.isLoading = false
+                    if (res.status === 200) {
+                        this.generalErrorMessage = res.data.message
+                        $('#generalModal').modal('show')
+                        this.email = ''
+                        this.password = ''
+                        this.confirm_password = ''
+                        setTimeout(() => {
+                            $('#generalModal').modal('hide')
+                            $('#staticBackdrop').modal('hide')
+                            $('#staticBackdrop2').modal('hide')
+                        }, 5000);
+                    }
+                })
+                .catch((err) => {
+                    this.isLoading = false
+                    if (err.response !== undefined) {
+                        this.generalErrorMessage = err.response
+                    } else {
+                        this.generalErrorMessage = err
+                    }
+                    $('#generalModal').modal('show')
+                    setTimeout(() => {
+                        $('#generalModal').modal('hide')
+                    }, 3000);
+                })
         },
         getListItems: function () {
             // let url = 'https://api.foodiew.com/getlist'
